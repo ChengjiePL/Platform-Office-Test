06ae766384e10fc1daf6c17720a7158ff67de862
Chengjie <163763355+ChengjiePL@users.noreply.github.com>
2026-07-21 23:23:12 +0200

    Delete azure-vm-poc directory

--- azure-vm-poc/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "public_ip_address" {
-  description = "Public IP address (if created)"
-  value       = var.public_ip ? azurerm_public_ip.pip[0].ip_address : ""
-}
-
-output "vm_name" {
-  value = azurerm_linux_virtual_machine.vm.name
-}
